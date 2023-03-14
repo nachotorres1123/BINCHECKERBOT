c7e33fe83551f6a5c0b0490df4250be0b216b1b5
FrankoSav <117610367+FrankoSav@users.noreply.github.com>
2023-03-14 05:55:28 +0100

    updated

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
 import json
-from flask import Flask
+from flask import Flask, request
+import telegram
+from telegram.ext import Dispatcher, CommandHandler
 import requests
 
 app = Flask(__name__)
@@ -12,35 +14,45 @@ logging.basicConfig(
 
 logger = logging.getLogger()
 
-def check_bin(update, context, args):
-    bin_number = args[0]
-    url = f"https://lookup.binlist.net/{bin_number}"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    if "valid" in data and data["valid"]:
-        card = data["card"]
-        issuer = data["issuer"]
-        country = data["country"]
-        message = "✅Coco valido.\n"
-        message += f"Esquema: {card['scheme']}\n"
-        message += f"Tipo: {card['type']}\n"
-        message += f"Categoria: {card['category']}\n"
-        message += f"Emisor: {issuer['name']}\n"
-        message += f"URL: {issuer['url']}\n"
-        message += f"Telefono: {issuer['tel']}\n"
-        message += f"País: {country['name']}\n"
-        message += f"Codigo Numerico: {country['numeric code']}\n"
-        message += f"Codigo Alpha 2: {country['alpha 2 code']}\n"
-        message += f"Latitud: {country['latitude']}\n"
-        message += f"Longitud: {country['longitude']}\n"
-        message += f"Moneda: {country['currency']}\n"
-        message += f"Nombre Moneda: {country['currency name']}\n"
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Hola, ¿en qué puedo ayudarte?")
+
+def check_bin(update, context):
+    args = context.args
+    if len(args) == 0:
+        message = "❌ Debes proporcionar un número de BIN."
     else:
-        message = "❌ El BIN ingresado no es válido."
+        bin_number = args[0]
+        url = f"https://lookup.binlist.net/{bin_number}"
+        response = requests.get(url)
+        data = json.loads(response.text)
+        if "valid" in data and data["valid"]:
+            card = data["card"]
+            issuer = data["issuer"]
+            country = data["country"]
+            message = "✅Coco valido.\n"
+            message += f"Esquema: {card['scheme']}\n"
+            message += f"Tipo: {card['type']}\n"
+            message += f"Categoria: {card['category']}\n"
+            message += f"Emisor: {issuer['name']}\n"
+            message += f"URL: {issuer['url']}\n"
+            message += f"Telefono: {issuer['tel']}\n"
+            message += f"País: {country['name']}\n"
+            message += f"Codigo Numerico: {country['numeric code']}\n"
+            message += f"Codigo Alpha 2: {country['alpha 2 code']}\n"
+            message += f"Latitud: {country['latitude']}\n"
+            message += f"Longitud: {country['longitude']}\n"
+            message += f"Moneda: {country['currency']}\n"
+            message += f"Nombre Moneda: {country['currency name']}\n"
+        else:
+            message = "❌ El BIN ingresado no es válido."
     context.bot.send_message(chat_id=update.message.chat_id, text=message)
 
+dispatcher = Dispatcher(bot=None, update_queue=None, use_context=True)
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("check_bin", check_bin))
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def webhook():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     dispatcher.process_update(update)
@@ -50,3 +62,8 @@ def main():
     config = Config()
     global bot
     bot = telegram.Bot(config.TOKEN)
+    dispatcher.bot = bot
+    app.run(host="0.0.0.0", port=config.PORT)
+
+if __name__ == "__main__":
+    main()
--- config.py
+++ config.py
@@ -1,10 +1,4 @@
 import os
-import json
-from queue import Queue
-from os import path, getenv
-
 
 class Config:
-    API_ID = getenv("ID", "ID DE DESARROLLADOR DE TELEGRAM")
-    API_HASH = getenv("HASH", "HASH DE LA API DE TELEGRAM")
-    BOT_TOKEN = getenv("TOKEN", "TOKEN DE BOT")
+    TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
